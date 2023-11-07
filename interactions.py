749c0f3fa6cfb2073fa647aa0c807e4b627a3157
Derek Pendleton <derekteepee@gmail.com>
2023-11-07 14:52:23 -0500

    added goal statement

--- interactions.py
+++ interactions.py
@@ -1,2 +1,7 @@
 from gpt4all import GPT4All
 
+# models will not be uploaded to the github repo to avoid any license issues.
+
+# the goal of this program is to perform speech recognition as input from the user.
+# then the user input will be sent to a local GPT model. After a response is generated,
+# the response will be vocalized and played back to the user.
